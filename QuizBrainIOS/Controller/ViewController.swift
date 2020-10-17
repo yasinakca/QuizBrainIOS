619aa5e06ad917b265c0a48f5006798a4847e0d4
yasinakca <yasinakcadev@gmail.com>
2020-10-18 00:38:55 +0300

    final

--- QuizBrainIOS/Controller/ViewController.swift
+++ QuizBrainIOS/Controller/ViewController.swift
@@ -8,12 +8,45 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var trueButton: UIButton!
+    @IBOutlet weak var falseButton: UIButton!
+    @IBOutlet weak var progressBar: UIProgressView!
+    
+    var quizBrain = QuizBrain()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        updateUI()
     }
-
-
+    
+    @IBAction func answerPressed(_ sender: UIButton) {
+        
+        if quizBrain.checkAnswer(userAnswer: sender.currentTitle!) == true{
+            trueButton.backgroundColor = UIColor.green
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.3) {
+                self.trueButton.backgroundColor = UIColor.clear
+            }
+        }else{
+            falseButton.backgroundColor = UIColor.red
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.3) {
+                self.falseButton.backgroundColor = UIColor.clear
+            }
+        }
+        
+        quizBrain.nextQuestion()
+        
+        updateUI()
+    }
+    
+    func updateUI(){
+        questionLabel.text = quizBrain.getQuestionText()
+        progressBar.progress = quizBrain.getProgress()
+        scoreLabel.text = "Score: \(quizBrain.getScore())"
+    }
+    
 }
 
--- QuizBrainIOS/Model/QuizBrain.swift
+++ QuizBrainIOS/Model/QuizBrain.swift
@@ -6,3 +6,46 @@
 //
 
 import Foundation
+struct QuizBrain {
+    var questions = [
+        Question(q: "q1",a: "True"),
+        Question(q: "q2",a: "False"),
+        Question(q: "q3",a: "True"),
+        Question(q: "q4",a: "False"),
+        Question(q: "q5",a: "True"),
+        Question(q: "q6",a: "False"),
+    ]
+    
+    var questionNumber = 0
+    var score = 0
+    
+    mutating func checkAnswer(userAnswer: String) -> Bool{
+        if questions[questionNumber].answer == userAnswer {
+            score += 1
+            return true
+        }else {
+            return false
+        }
+    }
+    
+    mutating func nextQuestion(){
+        if questionNumber + 1 < questions.count {
+            questionNumber += 1
+        }else{
+            questionNumber = 0
+            score = 0
+        }
+    }
+    
+    func getQuestionText() -> String{
+        return questions[questionNumber].text
+    }
+    
+    func getProgress() -> Float {
+        return Float(questionNumber + 1) / Float(questions.count)
+    }
+    
+    func getScore() -> Int{
+        return score
+    }
+}
